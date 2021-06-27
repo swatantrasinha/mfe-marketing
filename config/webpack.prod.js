2aca5227735ab6ae994060bf99b3865b5da26da1
Swatantra Sinha <swatantrasinha15aug@gmail.com>
2021-06-27 15:55:32 +0530

    ok

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,3 +1,4 @@
+console.log('######### yes prod config for webpack called !!!!');
 const {merge} = require('webpack-merge');
 const ModuleFederationPlugin= require('webpack/lib/container/ModuleFederationPlugin'); 
 const commonConfig = require('./webpack.common');
