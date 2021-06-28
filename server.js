579a49797b452da11e017574d896ee60c0885158
Swatantra Sinha <swatantrasinha15aug@gmail.com>
2021-06-28 19:35:57 +0530

    should work

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require('express');
 // const favicon = require('express-favicon');
 const path = require('path');
-const port = process.env.PORT || 8050;
+const port = process.env.PORT || 5000;
 const app = express();
 console.log('__dirname is : ', __dirname);
-// app.use(favicon(__dirname + '/public/favicon.png'));
-// the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname));
-console.log('__dirname is : ', __dirname);
+var distFolder = path.join(__dirname, 'dist');
+
+console.log('distFolder is : ', distFolder);
+
+app.use('/', express.static(distFolder));
 
 // send the user to index html page inspite of the url
 app.get('*', (req, res) => {
